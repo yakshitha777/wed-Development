e68f375f8a419fbe90a75ae892359a0658809b06
yakshitha777 <yakshitiruvaipati>
2024-09-11 10:58:51 +0530

    hi

---
+++ index.js
@@ -0,0 +1,82 @@
+let person={
+    name:"jack",
+    mark:700,
+    name:"boy",
+};
+console.log(person)
+person.mark=800
+person["name"]="rose"
+console.log(person)
+console.log(Object.keys(person))
+console.log(Object.entries(person))
+let colors=["red","blue"];
+colors[1]="yellow"
+console.log(colors)
+colors.push("green");
+console.log(colors);
+colors.pop()
+console.log(colors)
+colors.shift()
+console.log(colors)
+colors.unshift("purple")
+console.log(colors)
+console.log(colors.length);
+
+// function greet(params){
+    // console.log('function greet')
+// }
+// function add(p0,p2){
+    // return p0+p2;
+// }
+// greet()
+// let a=add(1,2)
+// console.log(a);
+// let input = prompt("enter input");
+// input=parseInt(input)
+// alert(typeof(input));
+
+function add(a,b){
+              
+    alert(a+b)
+}
+function sub(a,b){
+      
+      alert(a-b)
+  }
+  function mul(a,b){
+      
+      alert(a*b)
+  }
+  function div(a,b){
+      
+      alert(a/b)
+  }
+  function mod(a,b){
+      
+      alert(a%b)
+  }
+ let a=prompt('enter first number')
+ let b=prompt('enter second number')
+ let c=prompt('enter operator')
+ a=parseInt(a)
+ b=parseInt(b)
+ if(c=='+')
+ {
+    add(a,b);
+ }
+ if(c=='-')
+ {
+    sub(a,b);
+ }
+ if(c=='*')
+ {
+    mul(a,b);
+ }
+ if(c=='/')
+ {
+    div(a,b);
+ }
+ if(c=='%')
+ {
+    mod(a,b);
+ }
